ca5980a3315b7e5050c71510977f7f2a8916c758
Spence Churchill <spence@duck.com>
2022-06-08 14:12:58 -0700

    add clock-specific method

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var n = 0;
 // redraw the screen
 function draw() {
   g.reset().clearRect(Bangle.appRect);
-  g.setFont("6x8").setFontAlign(0,0).drawString("Up / Down",g.getWidth()/2,g.getHeight()/2 - 20);
-  g.setFont("Vector",60).setFontAlign(0,0).drawString(n,g.getWidth()/2,g.getHeight()/2 + 30);
+  g.setFont("6x8").setFontAlign(0, 0).drawString("Up / Down", g.getWidth() / 2, g.getHeight() / 2 - 20);
+  g.setFont("Vector", 60).setFontAlign(0, 0).drawString(n, g.getWidth() / 2, g.getHeight() / 2 + 30);
 }
 
 // Respond to user input
-Bangle.setUI({mode: "updown"}, function(dir) {
-  if (dir<0) {
+Bangle.setUI({ mode: "updown" }, function (dir) {
+  if (dir < 0) {
     n--;
     draw();
-  } else if (dir>0) {
+  } else if (dir > 0) {
     n++;
     draw();
   } else {
@@ -28,6 +28,9 @@ Bangle.setUI({mode: "updown"}, function(dir) {
 
 // First draw...
 draw();
+
+// Show launcher when middle button pressed
+Bangle.setUI("clock");
 
 // Load widgets
 Bangle.loadWidgets();
